a37f4748aa7076d3e8f2bc46fdece0c35154dd92
cantorfan <cantorfan@gmail.com>
2016-12-09 13:17:58 +0800

    OW-204

--- src/main/java/com/acuo/persist/entity/Agreement.java
+++ src/main/java/com/acuo/persist/entity/Agreement.java
@@ -1,6 +1,9 @@
 package com.acuo.persist.entity;
 
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.Set;
 
 @NodeEntity
 public class Agreement extends Entity {
@@ -15,5 +18,45 @@ public class Agreement extends Entity {
 
     public String getType() {
         return type;
+    }
+
+    @Relationship(type = "COUNTERPARTY_SIGNS", direction = Relationship.INCOMING)
+    private Set<LegalEntity> cptyEntitys;
+
+    @Relationship(type = "CLIENT_SIGNS", direction = Relationship.INCOMING)
+    private Set<LegalEntity> legalEntitys;
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Set<LegalEntity> getCptyEntitys() {
+        return cptyEntitys;
+    }
+
+    public void setCptyEntitys(Set<LegalEntity> cptyEntitys) {
+        this.cptyEntitys = cptyEntitys;
+    }
+
+    public Set<LegalEntity> getLegalEntitys() {
+        return legalEntitys;
+    }
+
+    public void setLegalEntitys(Set<LegalEntity> legalEntitys) {
+        this.legalEntitys = legalEntitys;
+    }
+
+    @Override
+    public String toString() {
+        return "Agreement{" +
+                "key='" + key + '\'' +
+                ", type='" + type + '\'' +
+                ", cptyEntitys=" + cptyEntitys +
+                ", legalEntitys=" + legalEntitys +
+                '}';
     }
 }
--- src/main/java/com/acuo/persist/entity/Client.java
+++ src/main/java/com/acuo/persist/entity/Client.java
@@ -1,6 +1,7 @@
 package com.acuo.persist.entity;
 
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
 import org.neo4j.ogm.annotation.Relationship;
 
 import java.util.Set;
@@ -8,9 +9,13 @@ import java.util.Set;
 @NodeEntity
 public class Client extends Entity {
 
+    @Property(name="id")
     private String clientId;
 
     private String name;
+
+    @Relationship(type = "DIRECTED_TO", direction = Relationship.INCOMING)
+    private Set<MarginStatement> marginStatements;
 
     public String getClientId() {
         return clientId;
@@ -25,5 +30,35 @@ public class Client extends Entity {
 
     public Set<Fund> getFunds() {
         return funds;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<MarginStatement> getMarginStatements() {
+        return marginStatements;
+    }
+
+    public void setMarginStatements(Set<MarginStatement> marginStatements) {
+        this.marginStatements = marginStatements;
+    }
+
+    public void setFunds(Set<Fund> funds) {
+        this.funds = funds;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "clientId='" + clientId + '\'' +
+                ", name='" + name + '\'' +
+                ", marginStatements=" + marginStatements +
+                ", funds=" + funds +
+                '}';
     }
 }
