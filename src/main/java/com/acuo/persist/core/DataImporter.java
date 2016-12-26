5a5aece44f324764f008e25b170a422f215ceaa5
Hicham Medkouri <hicham.medkouri@gmail.com>
2016-12-26 14:13:28 +0800

    OW-248 adding expected margin calls

--- src/main/java/com/acuo/persist/core/DataImporter.java
+++ src/main/java/com/acuo/persist/core/DataImporter.java
@@ -14,6 +14,7 @@ public interface DataImporter {
             "clearedAgreements",
             "bilateralAgreements",
             "mstatements",
+            "initmcexp",
             "initmc"
     };
 
