b09663df1eb7f7f26a459d35f3460773e9c0c57e
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-01-02 11:09:13 +0800

    OW-303 add a next relationship between step entities

--- src/main/java/com/acuo/persist/entity/Step.java
+++ src/main/java/com/acuo/persist/entity/Step.java
@@ -3,6 +3,7 @@ package com.acuo.persist.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
 
 @NodeEntity
 @Data
@@ -11,5 +12,6 @@ public class Step extends Entity{
 
     private String status;
 
-
+    @Relationship(type = "NEXT")
+    private Next next;
 }
