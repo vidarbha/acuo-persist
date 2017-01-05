62d0fb972b99854fb6047435dfd2889891d14140
cantorfan <cantorfan@gmail.com>
2017-01-05 16:40:24 +0800

    add Arriving

--- src/main/java/com/acuo/persist/entity/AssetTransferStatus.java
+++ src/main/java/com/acuo/persist/entity/AssetTransferStatus.java
@@ -1,6 +1,6 @@
 package com.acuo.persist.entity;
 
 public enum AssetTransferStatus {
-        Departed, InFlight,Delayed,Cancelled,Deployed,Available;
+        Departed, InFlight,Delayed,Cancelled,Deployed,Available,Arriving;
 
 }
