20b5e89af9801521572428363590e3766da6aace
cantorfan <cantorfan@gmail.com>
2017-01-05 17:54:58 +0800

    OW-243

---
+++ src/main/java/com/acuo/persist/entity/ClientAsset.java
@@ -0,0 +1,15 @@
+package com.acuo.persist.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+
+@NodeEntity
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ClientAsset extends Entity<ClientAsset> {
+
+    @Property(name = "id")
+    private String clientAssetId;
+}
