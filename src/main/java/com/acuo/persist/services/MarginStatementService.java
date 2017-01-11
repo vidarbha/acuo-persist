7b6aa92faa865d2f98b2a20a064d47697adc7587
cantorfan <cantorfan@gmail.com>
2017-01-11 23:11:46 +0800

    fix the recon expected

--- src/main/java/com/acuo/persist/services/MarginStatementService.java
+++ src/main/java/com/acuo/persist/services/MarginStatementService.java
@@ -1,6 +1,7 @@
 package com.acuo.persist.services;
 
 import com.acuo.persist.entity.CallStatus;
+import com.acuo.persist.entity.MarginCall;
 import com.acuo.persist.spring.Call;
 import com.acuo.persist.entity.MarginStatement;
 
@@ -13,5 +14,9 @@ public interface MarginStatementService extends Service<MarginStatement> {
     Iterable<Call> allCallsFor(String clientId, String dateTime);
 
     Iterable<MarginStatement> allStatementsForClient(String clientId);
+
+    Iterable<MarginStatement> allStatementsForRecon(String clientId);
+
+    Iterable<MarginCall> statementForExpected(String marginStatementId);
 
 }
--- src/main/java/com/acuo/persist/services/MarginStatementServiceImpl.java
+++ src/main/java/com/acuo/persist/services/MarginStatementServiceImpl.java
@@ -1,6 +1,7 @@
 package com.acuo.persist.services;
 
 import com.acuo.persist.entity.CallStatus;
+import com.acuo.persist.entity.MarginCall;
 import com.acuo.persist.spring.Call;
 import com.acuo.persist.entity.MarginStatement;
 import com.google.common.collect.ImmutableMap;
@@ -49,5 +50,24 @@ public class MarginStatementServiceImpl extends GenericService<MarginStatement> 
                         "WITH {category:a.type, type:mc.callType, status:step.status, balance: mc.balanceAmount, excess: mc.excessAmount} AS Call " +
                         "RETURN Call";
         return session.query(Call.class, query, ImmutableMap.of("clientId", clientId, "dateTime", dateTime));
+    }
+
+    @Override
+    public Iterable<MarginStatement> allStatementsForRecon(String clientId)
+    {
+        String query = "MATCH (:Client {id:{clientId}})-[:MANAGES]->(l:LegalEntity)-[]->(a:Agreement)<-[:STEMS_FROM]-(m:MarginStatement) " +
+                "WITH m " +
+                "MATCH p=(:Firm)-[:MANAGES]->(l:LegalEntity)-[]->(a:Agreement)<-[]-(m)<-[]-(mc:MarginCall)-[:LAST]->(step:Step {status:'Unrecon'}) " +
+                "RETURN m, nodes(p), rels(p)";
+        return session.query(MarginStatement.class, query, ImmutableMap.of("clientId", clientId));
+    }
+
+    @Override
+    public Iterable<MarginCall> statementForExpected(String marginStatementId)
+    {
+        String query = "MATCH (:Firm)-[:MANAGES]->(l:LegalEntity)-[]->(a:Agreement)<-[]-(m:MarginStatement {id:{msId}})<-[]-(mc1:MarginCall)-[:LAST]->(step:Step {status:'Unrecon'})" +
+                "MATCH (mc2:MarginCall)<-[MATCHED_TO_EXPECTED]-(mc1) " +
+                "return mc2";
+        return session.query(MarginCall.class, query, ImmutableMap.of("msId", marginStatementId));
     }
 }
