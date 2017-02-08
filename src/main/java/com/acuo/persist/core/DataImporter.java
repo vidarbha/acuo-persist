1a607ed3e38429e19fc7f6df3c0641d2ded82664
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-02-08 18:57:36 +0000

    OW-435 added buildEligibility load file in the reload sequence

--- src/main/java/com/acuo/persist/core/DataImporter.java
+++ src/main/java/com/acuo/persist/core/DataImporter.java
@@ -18,6 +18,7 @@ public interface DataImporter {
             "custodianAccounts",
             "counterpartCustodianAccounts",
             "custodianAssets",
+            "buildEligibility",
             "mstatements",
             "initmcexp",
             "initmc",
