38ed62177432eb8e8d1cde4f406660a88747d2d3
cantorfan <cantorfan@gmail.com>
2017-02-28 10:11:27 +0800

    OW-357

--- src/main/java/com/acuo/persist/entity/InitialMargin.java
+++ src/main/java/com/acuo/persist/entity/InitialMargin.java
@@ -1,8 +1,12 @@
 package com.acuo.persist.entity;
 
+import com.acuo.common.model.margin.Types;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.neo4j.ogm.annotation.NodeEntity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @NodeEntity
 @Data
@@ -13,5 +17,14 @@ public class InitialMargin extends MarginCall {
     private Double initialBalanceCash;
     private Double initialBalanceNonCash;
     private String IMRole;
+
+    public InitialMargin(String marginCallId , LocalDate callDate, Types.MarginType marginType, String direction , LocalDate valuationDate, String currency,
+                         Double excessAmount, Double balanceAmount, Double deliverAmount, Double returnAmount, Double pendingCollateral, Double exposure, Integer parentRank,
+                         LocalDateTime notificationTime, Double marginAmount, String status)
+    {
+        super(marginCallId, callDate,marginType,direction,valuationDate,currency,
+                excessAmount,balanceAmount, deliverAmount, returnAmount, pendingCollateral, exposure, parentRank,
+                notificationTime,marginAmount,status);
+    }
 
 }
