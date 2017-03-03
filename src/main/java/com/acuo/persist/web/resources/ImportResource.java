a6cb73bf9af586fb547216e23ddbbe703c2857a9
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-03-03 13:59:33 +0000

    OW-441 changed import service to specify the branch from where to load data files

--- src/main/java/com/acuo/persist/web/resources/ImportResource.java
+++ src/main/java/com/acuo/persist/web/resources/ImportResource.java
@@ -6,7 +6,7 @@ import com.codahale.metrics.annotation.Timed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -21,17 +21,17 @@ public class ImportResource {
     }
 
     @GET
-    @Path("reload/{branch}")
+    @Path("reload")
     @Timed
-    public Response reload(@PathParam("branch") String value) {
+    public Response reload(@QueryParam("branch") String value) {
         service.branch(value).reload();
         return Response.status(Status.OK).build();
     }
 
     @GET
-    @Path("load/{branch}/{fileName}")
+    @Path("load")
     @Timed
-    public Response load(@PathParam("branch") String branch, @PathParam("fileName") String fileName) {
+    public Response load(@QueryParam("branch") String branch, @QueryParam("file") String fileName) {
         service.branch(branch).load(fileName);
         return Response.status(Status.OK).build();
     }
