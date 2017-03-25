0039eac9205ef848ecb9f18b1f1903b5ae729c3a
cantorfan <cantorfan@gmail.com>
2017-03-25 12:45:33 +0800

    fix bug for 505

--- src/main/java/com/acuo/persist/entity/AssetValue.java
+++ src/main/java/com/acuo/persist/entity/AssetValue.java
@@ -2,11 +2,11 @@ package com.acuo.persist.entity;
 
 import com.acuo.persist.neo4j.converters.CurrencyConverter;
 import com.acuo.persist.neo4j.converters.LocalDateConverter;
-import com.opengamma.strata.basics.currency.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.typeconversion.Convert;
+import com.opengamma.strata.basics.currency.Currency;
 
 import java.time.LocalDate;
 import java.util.*;
