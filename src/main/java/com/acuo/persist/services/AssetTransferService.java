131064d606bd2c4112e7d336c460bd765d7256aa
cantorfan <cantorfan@gmail.com>
2017-04-09 17:49:31 +0800

    OW-385, add query method

--- src/main/java/com/acuo/persist/services/AssetTransferService.java
+++ src/main/java/com/acuo/persist/services/AssetTransferService.java
@@ -1,6 +1,9 @@
 package com.acuo.persist.services;
 
 import com.acuo.persist.entity.AssetTransfer;
+import org.neo4j.ogm.model.Result;
 
 public interface AssetTransferService extends Service<AssetTransfer>{
+
+    Result getPledgedAssets();
 }
