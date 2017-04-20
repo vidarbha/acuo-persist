f65069b50bd86fecd3b28c8520f26048780a5d3a
Hicham Medkouri <hicham@anaxo.io>
2017-04-20 11:27:04 +0100

    OW-561 fixing the eligible asset by call id query following recent changes

--- src/main/java/com/acuo/persist/services/AssetServiceImpl.java
+++ src/main/java/com/acuo/persist/services/AssetServiceImpl.java
@@ -15,15 +15,13 @@ public class AssetServiceImpl extends GenericService<Asset> implements AssetServ
             "RETURN asset, nodes(path), relationships(path), nodes(transfer), relationships(transfer)";
 
     private static String ELIGIBLE_ASSET_BY_CLIENT_AND_CALLID =
-            "MATCH (client:Client {id:{clientId}})-[:MANAGES]->(entity:LegalEntity)-[:HAS]->(:TradingAccount)" +
-            "-[:ACCESSES]->(ca:CustodianAccount)-[holds:HOLDS]->(asset:Asset)-[is:IS_AVAILABLE_FOR]->(agreement:Agreement)" +
-            "<-[:STEMS_FROM]-(ms:MarginStatement)<-[*1..2]-(marginCall:MarginCall {id:{callId}}), " +
-            "(entity)-[:CLIENT_SIGNS]-(agreement), " +
-            "(ms)-[:DIRECTED_TO]->(entity) " +
+            "MATCH (client:Client {id:{clientId}})-[:MANAGES]->(entity:LegalEntity)-[:CLIENT_SIGNS]->(agreement:Agreement)<-[is:IS_AVAILABLE_FOR]-(asset:Asset) " +
+            "WITH asset, client, agreement, entity, is " +
+            "MATCH (agreement)<-[:STEMS_FROM]-(ms:MarginStatement)<-[*1..2]-(marginCall:MarginCall {id:'mcp26'}),(ms)-[:DIRECTED_TO]->(entity) " +
             "WHERE marginCall.marginType IN is.marginType " +
             "AND NOT (asset)-[:EXCLUDED]->(marginCall) " +
-            "WITH DISTINCT asset, ca, is " +
-            "MATCH path=(Custodian)-[MANAGES]->(ca)-[holds]->(asset)-[is]-() " +
+            "WITH DISTINCT asset " +
+            "MATCH path=(ca:CustodianAccount)-[holds]->(asset)-[:VALUATED]->(valuation:Valuation)-[:VALUE]->(value:AssetValue) " +
             "RETURN asset, nodes(path), relationships(path)";
 
     @Override
