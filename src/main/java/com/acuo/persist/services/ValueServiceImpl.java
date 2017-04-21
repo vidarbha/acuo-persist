1366401a88edaa60141ae6ad8bc52975ac5e36f4
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-04-21 21:04:01 +0100

    OW-566 changed type of the ValueService ID from String to Long

--- src/main/java/com/acuo/persist/services/ValueServiceImpl.java
+++ src/main/java/com/acuo/persist/services/ValueServiceImpl.java
@@ -2,7 +2,7 @@ package com.acuo.persist.services;
 
 import com.acuo.persist.entity.Value;
 
-public class ValueServiceImpl extends GenericService<Value, String> implements  ValueService{
+public class ValueServiceImpl extends GenericService<Value, Long> implements  ValueService{
 
     @Override
     public Class<Value> getEntityType() {
