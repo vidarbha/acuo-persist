fd0904a86500aef6619ea35055eccb0220e61775
Hicham Medkouri <hicham@anaxo.io>
2017-04-27 08:40:38 +0100

    OW-535 statement item ids are now define on the parent class StatementItem

--- src/main/java/com/acuo/persist/entity/InterestPayment.java
+++ src/main/java/com/acuo/persist/entity/InterestPayment.java
@@ -2,16 +2,11 @@ package com.acuo.persist.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.neo4j.ogm.annotation.Index;
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Property;
 
 @NodeEntity
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class InterestPayment extends StatementItem<InterestPayment> {
 
-    @Property(name="id")
-    @Index(primary = true)
-    private String interestPaymentId;
 }
