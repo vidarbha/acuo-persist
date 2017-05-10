ebe934321c33d5ac8360d840425b60cc516c6b04
Hicham Medkouri <hicham@anaxo.io>
2017-05-10 18:29:19 +0100

    OW-586 handle queen currencies for fx rate service

--- src/main/java/com/acuo/persist/services/CurrencyServiceImpl.java
+++ src/main/java/com/acuo/persist/services/CurrencyServiceImpl.java
@@ -22,8 +22,8 @@ public class CurrencyServiceImpl extends GenericService<CurrencyEntity, Long> im
     @Override
     public Double getFXValue(Currency currency) {
         String query =
-                "MATCH (c:Currency{id:{id}})-[r:FX_RATE]->(usd:Currency {id:'USD'}) " +
-                        "RETURN r.fxRate";
+                "MATCH (c:Currency{id:{id}})-[r:FX_RATE]-(usd:Currency {id:'USD'}) " +
+                "RETURN CASE WHEN STARTNODE(r) = c THEN r.fxRate ELSE 1/r.fxRate END";
         return sessionProvider.get().queryForObject(Double.class, query, ImmutableMap.of("id", currency.getCode()));
     }
 
@@ -32,8 +32,9 @@ public class CurrencyServiceImpl extends GenericService<CurrencyEntity, Long> im
     public Map<Currency, Double> getAllFX() {
         Map<Currency, Double> values = new HashMap<>();
         String query =
-                "MATCH (c:Currency)-[r:FX_RATE]->(usd:Currency {id:'USD'}) " +
-                        "RETURN c.id as id, r.fxRate as rate";
+                "MATCH (c:Currency)-[r:FX_RATE]-(usd:Currency {id:'USD'}) " +
+                "RETURN c.id as id, CASE WHEN STARTNODE(r) = c THEN r.fxRate ELSE 1/r.fxRate END AS rate " +
+                "ORDER BY id";
         Result result = sessionProvider.get().query(query, Collections.emptyMap());
         result.forEach(map -> values.put(Currency.of((String) map.get("id")), (Double) map.get("rate")));
         return values;
