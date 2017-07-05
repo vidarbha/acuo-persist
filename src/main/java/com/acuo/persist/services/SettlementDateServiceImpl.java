92f92d4fa0617df714b0bef02ffa275f0148170e
cantorfan <cantorfan@gmail.com>
2017-07-05 20:59:24 +0800

    OW-676, add SettlementDateService.java

---
+++ src/main/java/com/acuo/persist/services/SettlementDateServiceImpl.java
@@ -0,0 +1,11 @@
+package com.acuo.persist.services;
+
+import com.acuo.persist.entity.SettlementDate;
+
+public class SettlementDateServiceImpl extends GenericService<SettlementDate, String> implements SettlementDateService{
+
+    @Override
+    public Class<SettlementDate> getEntityType() {
+        return SettlementDate.class;
+    }
+}
