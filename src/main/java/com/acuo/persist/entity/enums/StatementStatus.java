5eae0a68200b99ca6d505ff42e89f0d6fc5ab2c4
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-10-05 13:22:52 +0100

    OW-363 adding a Sent statement status

--- src/main/java/com/acuo/persist/entity/enums/StatementStatus.java
+++ src/main/java/com/acuo/persist/entity/enums/StatementStatus.java
@@ -1,5 +1,5 @@
 package com.acuo.persist.entity.enums;
 
 public enum StatementStatus {
-    Received, Expected, Unrecon, MatchedToReceived, Reconciled, Pledged, WaitDispute, ActionDispute, PartialDispute, Closed, Error, Cancelled, MatchedToSent
+    Received, Expected, Sent, Unrecon, MatchedToReceived, Reconciled, Pledged, WaitDispute, ActionDispute, PartialDispute, Closed, Error, Cancelled, MatchedToSent
 }
