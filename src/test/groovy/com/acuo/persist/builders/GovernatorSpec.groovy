2d3d5ae3865ccfe4b02b24fb813eb7471a2b140c
Hicham Medkouri <hicham@anaxo.io>
2017-10-06 12:20:26 +0100

    OW-363 adding margin statements and agreements builders

--- src/test/groovy/com/acuo/persist/builders/GovernatorSpec.groovy
+++ src/test/groovy/com/acuo/persist/builders/GovernatorSpec.groovy
@@ -2,9 +2,11 @@ package com.acuo.persist.builders
 
 import com.netflix.governator.guice.test.InjectorCreationMode
 import com.netflix.governator.guice.test.ModulesForTesting
+import spock.lang.Ignore
 import spock.lang.Specification
 
 @ModulesForTesting(injectorCreation = InjectorCreationMode.BEFORE_EACH_TEST_METHOD)
+@Ignore("ignore until governator fix https://github.com/Netflix/governator/issues/372")
 class GovernatorSpec extends Specification {
 
     def "dummy test"() {
