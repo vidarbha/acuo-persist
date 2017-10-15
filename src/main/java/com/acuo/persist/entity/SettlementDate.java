2b51820ce82fa4158ba469a30b4028ae0b592b24
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-10-15 20:41:17 +0100

    OW-816 added a creation date and time property to the settlement date

--- src/main/java/com/acuo/persist/entity/SettlementDate.java
+++ src/main/java/com/acuo/persist/entity/SettlementDate.java
@@ -4,18 +4,13 @@ import com.acuo.persist.neo4j.converters.LocalDateConverter;
 import com.acuo.persist.neo4j.converters.LocalDateTimeConverter;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import org.neo4j.causalclustering.core.consensus.outcome.Outcome;
-import org.neo4j.cypher.internal.frontend.v3_1.SemanticDirection;
 import org.neo4j.ogm.annotation.Index;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Property;
-import org.neo4j.ogm.annotation.Relationship;
 import org.neo4j.ogm.annotation.typeconversion.Convert;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @NodeEntity
 @Data
