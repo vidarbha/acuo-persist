2b7071636655f791e4ae6292bc0a60d770d00332
Hicham Medkouri <hicham@anaxo.io>
2017-10-23 08:09:22 +0100

    OW-864 Retrieve asset and call FX rates

--- src/main/java/com/acuo/persist/services/AssetTransferServiceImpl.java
+++ src/main/java/com/acuo/persist/services/AssetTransferServiceImpl.java
@@ -180,12 +180,13 @@ public class AssetTransferServiceImpl extends AbstractService<AssetTransfer, Str
 
     public Result getPledgedAssets() {
         String query =
-        "MATCH (asset:Asset)<-[:OF]-(at:AssetTransfer {status:'Departed'})-[:GENERATED_BY]->(call:MarginCall)\n" +
-        "MATCH (call)-[:PART_OF]->(:MarginStatement)-[:STEMS_FROM]->(a:Agreement) \n" +
-        "MATCH (l1:LegalEntity)-[:CLIENT_SIGNS]->(a)<-[:COUNTERPARTY_SIGNS]-(l2:LegalEntity) \n" +
-        "MATCH (c1:Custodian)-[:MANAGES]->(ca1:CustodianAccount)<-[:FROM]-(at)-[:TO]->(ca2:CustodianAccount)<-[:MANAGES]-(c2:Custodian)\n" +
-        "RETURN at.id, at.pledgeTime, a.name, l1.name, l2.name, a.currency, at.subStatus, c1.name, " +
-        "c2.name, ca1.name, ca2.name, at.quantity, asset.currency,asset.name, asset.id, asset.settlementTime, at.settlementDate";
+        "MATCH (asset:Asset)<-[:OF]-(at:AssetTransfer {status:'Departed'})-[:GENERATED_BY]->(call:MarginCall) " +
+        "MATCH (call)-[:PART_OF]->(:MarginStatement)-[:STEMS_FROM]->(a:Agreement) " +
+        "MATCH (l1:LegalEntity)-[:CLIENT_SIGNS]->(a)<-[:COUNTERPARTY_SIGNS]-(l2:LegalEntity) " +
+        "MATCH (c1:Custodian)-[:MANAGES]->(ca1:CustodianAccount)<-[:FROM]-(at)-[:TO]->(ca2:CustodianAccount)<-[:MANAGES]-(c2:Custodian) " +
+        "RETURN a.name, a.currency, l1.name, l2.name, c1.name, c2.name, ca1.name, ca2.name, " +
+        "at.id, at.pledgeTime, at.unitValue, at.quantity, at.subStatus, at.settlementDate, at.assetFxRate, at.callFxRate " +
+        "asset.currency, asset.name, asset.id, asset.settlementTime";
         return dao.getSession().query(query, Collections.emptyMap());
     }
 }
