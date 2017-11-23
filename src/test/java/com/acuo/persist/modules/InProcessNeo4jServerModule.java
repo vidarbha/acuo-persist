4aa3e4f734cb30b20a846b5b159026517547bd46
Hicham Medkouri <hicham@anaxo.io>
2017-11-23 09:54:31 +0000

    OW-835 updated neo4j and ogm version to the latest stable

--- src/test/java/com/acuo/persist/modules/InProcessNeo4jServerModule.java
+++ src/test/java/com/acuo/persist/modules/InProcessNeo4jServerModule.java
@@ -1,62 +1,61 @@
 package com.acuo.persist.modules;
 
+import com.acuo.common.util.AnnotationsModule;
 import com.acuo.persist.core.SingleNeo4jSessionFactory;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import org.neo4j.backup.OnlineBackupSettings;
 import org.neo4j.harness.ServerControls;
-import org.neo4j.harness.TestServerBuilder;
 import org.neo4j.harness.TestServerBuilders;
 import org.neo4j.kernel.configuration.Settings;
 import org.neo4j.ogm.config.Configuration;
 import org.neo4j.ogm.session.SessionFactory;
 
-public class InProcessNeo4jServerModule extends AbstractModule {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Singleton;
 
-    private static ServerControls controls;
-    static {
-        TestServerBuilder builder = TestServerBuilders.newInProcessBuilder();
-        builder.withConfig(OnlineBackupSettings.online_backup_enabled, Settings.FALSE);
-        controls = builder.newServer();
-    }
+public class InProcessNeo4jServerModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        Configuration configuration = new Configuration.Builder()
-                .uri(controls.boltURI().toString())
-                .connectionPoolSize(150)
-                .encryptionLevel("NONE")
-                .build();
 
-        bind(Configuration.class).toInstance(configuration);
+        install(new AnnotationsModule());
+
         bind(SessionFactory.class).toProvider(SingleNeo4jSessionFactory.class);
     }
 
+    @Provides
+    Configuration configuration(CloseableTestServer server) {
+        Configuration configuration = new Configuration.Builder()
+                .uri(server.controls().boltURI().toString())
+                .connectionPoolSize(150)
+                .encryptionLevel("NONE")
+                .build();
+        return configuration;
+    }
 
-//
-//    @Singleton
-//    static class CloseableTestServer {
-//
-//        private final TestServerBuilder builder = EnterpriseTestServerBuilders.newInProcessBuilder();
-//        private ServerControls serverControls = null;
-//
-//        public CloseableTestServer() {
-//            builder.withConfig(OnlineBackupSettings.online_backup_enabled, Settings.FALSE);
-//        }
-//
-//        @PostConstruct
-//        public void start() {
-//            serverControls = builder.newServer();
-//        }
-//
-//        @PreDestroy
-//        public void stop() {
-//            if (serverControls != null) {
-//                serverControls.close();
-//            }
-//        }
-//
-//        public ServerControls controls() {
-//            return serverControls;
-//        }
-//    }
+    @Singleton
+    static class CloseableTestServer {
+
+        private ServerControls serverControls = null;
+
+        @PostConstruct
+        public void start() {
+            serverControls = TestServerBuilders.newInProcessBuilder()
+                            .withConfig(OnlineBackupSettings.online_backup_enabled, Settings.FALSE)
+                            .newServer();
+        }
+
+        @PreDestroy
+        public void stop() {
+            if (serverControls != null) {
+                serverControls.close();
+            }
+        }
+
+        public ServerControls controls() {
+            return serverControls;
+        }
+    }
 }
